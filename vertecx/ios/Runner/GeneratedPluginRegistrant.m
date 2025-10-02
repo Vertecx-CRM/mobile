9c41db260ba8db598dc99d7304dd8328fb185b64
JoaoOrtz <joaoestid@gmail.com>
2025-10-01 19:16:56 -0500

    fix: corregir bug en la lógica de visualización de alertas

--- vertecx/ios/Runner/GeneratedPluginRegistrant.m
+++ vertecx/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,16 +6,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<rive_native/RiveNativePlugin.h>)
-#import <rive_native/RiveNativePlugin.h>
-#else
-@import rive_native;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [RiveNativePlugin registerWithRegistrar:[registry registrarForPlugin:@"RiveNativePlugin"]];
 }
 
 @end
