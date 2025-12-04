dee3006d772af563161e5efe111e08edf947b1ef
kenyahpalacios21 <kenyahpalacios4@gmail.com>
2025-12-03 22:33:57 -0500

    fix: arreglé la url del backend de mis módulos de técnicos y roles (estaban mal apuntadas, al parecer los cambiaron)

--- vertecx/lib/data/services/technicians_service.dart
+++ vertecx/lib/data/services/technicians_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 import 'package:vertecx/data/models/technicians/technician_model.dart';
 
 class TechniciansService {
-  final String baseUrl = "http://192.168.1.9:3001";
+  final String baseUrl = "http://192.168.1.54:3001";
 
   final http.Client _client;
 
